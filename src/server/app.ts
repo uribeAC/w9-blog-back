6a4c1d30ec90c159d292cbe83e9af616bf712cff
Alex Uribe <repzyyog@gmail.com>
2025-04-22 20:08:48 +0200

    Delete dead code in app

--- src/server/app.ts
+++ src/server/app.ts
@@ -6,10 +6,6 @@ const app = express();
 
 app.use(morgan("dev"));
 
-app.get("/pokemons", (req, res, next) => {
-  next(new Error("pokemon dosent exist"));
-});
-
 app.use(handleErrors);
 
 export default app;

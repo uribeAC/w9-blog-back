e4633e5697d172373696ef346dad485e57fd8ea1
Alex Uribe <repzyyog@gmail.com>
2025-04-22 20:01:53 +0200

    Add tests to handleErrors middleware

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "jest";
+import { createDefaultPreset } from "ts-jest";
+
+const config: Config = {
+  verbose: true,
+  rootDir: "src",
+  resolver: "ts-jest-resolver",
+  ...createDefaultPreset(),
+};
+
+export default config;

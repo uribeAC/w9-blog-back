96662d40b510e2009b681439add2947bf27a04be
Ivet Lopez <ivet19@live.com>
2025-04-23 16:53:51 +0200

    Fix typo error and divide test in two

--- src/server/middlewares/handleEndpointNotFound/handleEndpointNotFound.test.ts
+++ src/server/middlewares/handleEndpointNotFound/handleEndpointNotFound.test.ts
@@ -3,8 +3,8 @@ import { NextFunction, Request, Response } from "express";
 import handleEndpointNotFound from "./handleEndpointNotFound.js";
 
 describe("Given the handleEndpointNotFound middleware", () => {
-  describe("When it recieives a next function", () => {
-    test("Then it should call the next function with an error 'Endpoint not found' and a status code '404'", () => {
+  describe("When it recieves a next function", () => {
+    test("Then it should call the next function with an error 'Endpoint not found'", () => {
       const error = new ServerError(404, "Endpoint not found");
 
       const next = jest.fn();
@@ -18,6 +18,19 @@ describe("Given the handleEndpointNotFound middleware", () => {
       expect(next).toHaveBeenCalledWith(
         expect.objectContaining({ message: error.message }),
       );
+    });
+
+    test("Then it should call the next function with a status code '404'", () => {
+      const error = new ServerError(404, "Endpoint not found");
+
+      const next = jest.fn();
+
+      handleEndpointNotFound(
+        {} as Request,
+        {} as Response,
+        next as NextFunction,
+      );
+
       expect(next).toHaveBeenCalledWith(
         expect.objectContaining({ statusCode: error.statusCode }),
       );

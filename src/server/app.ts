c3a7df9f3afba28785bb01eab0a2bd8e784c0fb1
Isabel Sáenz <isasaete@gmail.comn>
2025-04-23 19:51:31 +0200

    Add postRouter

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,14 +3,17 @@ import morgan from "morgan";
 import handleEndpointNotFound from "./middlewares/handleEndpointNotFound/handleEndpointNotFound.js";
 import handleErrors from "./middlewares/handleErrors/handleErrors.js";
 import checkHealthStatus from "./middlewares/checkHealthStatus/checkHealthStatus.js";
+import postsRouter from "../post/router/postsRouter.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.use(handleEndpointNotFound);
+app.get("/", checkHealthStatus);
 
-app.use("/", checkHealthStatus);
+app.use("/posts", postsRouter);
+
+app.use(handleEndpointNotFound);
 
 app.use(handleErrors);
 

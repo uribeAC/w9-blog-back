8ea7df88a75181a72a88cf6ea8d8b58373e4f9aa
Alex Uribe <repzyyog@gmail.com>
2025-04-29 15:20:27 +0200

    Added app to test suit

--- src/server/__tests__/checkHealthStatus.test.ts
+++ src/server/__tests__/checkHealthStatus.test.ts
@@ -1,5 +1,46 @@
 import request from "supertest";
-import app from "../app.js";
+import express from "express";
+import cors from "cors";
+import handleErrors from "../middlewares/handleErrors/handleErrors.js";
+import checkHealthStatus from "../middlewares/checkHealthStatus/checkHealthStatus.js";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(
+  cors({
+    origin(requestOrigin, callback) {
+      const allowedOriginPatterns = process.env.ALLOWED_ORIGIN_PATTERNS;
+
+      if (!allowedOriginPatterns) {
+        callback(new Error("Not allowed origins"));
+        return;
+      }
+
+      const originMatches = allowedOriginPatterns.split(",").some((pattern) => {
+        const result = new RegExp(pattern).test(requestOrigin!);
+        return result;
+      });
+
+      if (!requestOrigin || originMatches) {
+        callback(null, true);
+      } else {
+        callback(new Error("Origin not allowed"));
+      }
+    },
+    credentials: true,
+  }),
+);
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.get("/", checkHealthStatus);
+
+app.use(handleErrors);
+
+export default app;
 
 describe("Given a GET / endpoint", () => {
   describe("When it receives a request", () => {

1fb7317f2f41c3f95b18caf8659bf78b4608e0fa
Alex Uribe <repzyyog@gmail.com>
2025-05-04 12:50:04 +0200

    Add isValidId middleware to postsRouter

--- src/post/router/postsRouter.ts
+++ src/post/router/postsRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import PostController from "../controller/PostController.js";
 import Post from "../model/Post.js";
+import isValidId from "../../server/middlewares/isValidId/isValidId.js";
 
 const postsRouter = Router();
 
@@ -8,10 +9,10 @@ const postController = new PostController(Post);
 
 postsRouter.get("/", postController.getPostsPage);
 
-postsRouter.get("/:postId", postController.getPostById);
+postsRouter.get("/:postId", isValidId, postController.getPostById);
 
 postsRouter.post("/", postController.addPost);
 
-postsRouter.delete("/:postId", postController.deletePost);
+postsRouter.delete("/:postId", isValidId, postController.deletePost);
 
 export default postsRouter;
